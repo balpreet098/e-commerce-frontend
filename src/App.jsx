f5fbe4720d4a11f044b00c94d6a360133672000d
balpreet098 <balpreet.immanent@gmail.com>
2024-08-12 18:22:22 +0530

    second update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+import { useState } from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import BrowserRouter and Route components
+import Accounts from './UserLayout/pages/Accounts/Accounts'; // Imported as Accounts
+import Cart from './UserLayout/pages/Cart/Cart';
+import Home from './UserLayout/pages/Homepage/Home';
+import Login from './UserLayout/pages/Login/Login';
+import Orders from './UserLayout/pages/Orders/Orders';
+import Products from './UserLayout/pages/Products/Products';
+import Register from './UserLayout/pages/Register/Register';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
- <>
-<Navbar/>
-
-
- </>
-  )
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          {/* Define your routes here */}
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/account" element={<Accounts />} /> {/* Use Accounts component */}
+          <Route path="/orders" element={<Orders />} />
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,34 +1,46 @@
 import React from "react";
+import { NavLink } from "react-router-dom"; // Import NavLink
 import "./Navbar.css";
+import logo from '../../assets/img/hi.png'; // Import the image
+
 function Navbar() {
   return (
-    <>
-      <div className="nav-container">
-        <div className="left-container">
-          <div className="left-logo">Logo</div>
-          <div className="left-com-name">Company Name</div>
+    <div className="nav-container">
+      <div className="left-container">
+        <div className="left-logo">
+          <img src={logo} style={{ width: "100px" }} alt="Logo" /> {/* Use imported image */}
         </div>
+        <div className="left-com-name"><h1>SHOPPERS</h1></div>
+      </div>
 
+      <div className="searchBar">
+        <input className="searchInput" type="text" />
+      </div>
 
-        <div className="searchBar">
-            <input type="text" />
+      <div className="right-container">
+        <div className="right-link">
+          <NavLink className="list-item" to="/" end>
+            Home
+          </NavLink>
+          <NavLink className="list-item" to="/products">
+            Products
+          </NavLink>
+          <NavLink className="list-item" to="/cart">
+            Cart
+          </NavLink>
+          <NavLink className="list-item" to="/account">
+            Account
+          </NavLink>
+          <NavLink className="list-item" to="/orders">
+            Orders
+          </NavLink>
         </div>
-
-        <div className="right-container">
-          <div className="right-link">
-            <li className="list-item">Home</li>
-            <li className="list-item">Products</li>
-            <li className="list-item">Cart</li>
-            <li className="list-item">Account</li>
-            <li className="list-item">Orders</li>
-          </div>
-          <div className="right-user-info">
-            Welcome, Ballu Bhoi
-            <span>Logo</span>
-          </div>
+        <div className="right-user-info">
+          Welcome, Ballu Bhoi
+          <span>Logo</span>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
